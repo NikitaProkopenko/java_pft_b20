0f4d8883ce623f99153adf4ce59a6dcc6408321e
Nikita Prokopenko <guardprokopenko@gmail.com>
2020-10-25 12:58:58 +0300

    Switched to the method for the distance calculation

--- sandbox/src/main/java/ru/stqa/pft/sandbox/FirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/FirstProgram.java
@@ -1,17 +1,14 @@
 package ru.stqa.pft.sandbox;
 
+import static ru.stqa.pft.sandbox.Point.distance;
+
 public class FirstProgram {
     public static void main(String[] args) {
+
         Point p1 = new Point(2.0, 3.0);
         Point p2 = new Point(5.0, 8.0);
 
-        System.out.println(distance(p1, p2));
+        System.out.println("The distance between point p1 and p2: " + distance(p1, p2));
     }
 
-
-
-    public static double distance(Point p1, Point p2) {
-        double result = Math.sqrt(((p2.x - p1.x) * (p2.x - p1.x)) + ((p2.y - p1.y) * (p2.y - p1.y)));
-        return result;
-    }
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -10,5 +10,9 @@ public class Point {
         this.y = y;
     }
 
+    public static double distance(Point p1, Point p2) {
+        double result = Math.sqrt(((p2.x - p1.x) * (p2.x - p1.x)) + ((p2.y - p1.y) * (p2.y - p1.y)));
+        return result;
+    }
 
 }
