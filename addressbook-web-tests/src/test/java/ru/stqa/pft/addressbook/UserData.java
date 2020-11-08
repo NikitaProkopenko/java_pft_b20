ec39b0ae030790f5c08ba424a7a91c2d9a375132
Nikita Prokopenko <guardprokopenko@gmail.com>
2020-11-08 19:44:26 +0300

    Added test for new user creation

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/UserData.java
@@ -0,0 +1,31 @@
+package ru.stqa.pft.addressbook;
+
+public class UserData {
+    private final String firstName;
+    private final String lastName;
+    private final String homePhone;
+    private final String email;
+
+    public UserData(String firstName, String lastName, String homePhone, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.homePhone = homePhone;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getHomePhone() {
+        return homePhone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
