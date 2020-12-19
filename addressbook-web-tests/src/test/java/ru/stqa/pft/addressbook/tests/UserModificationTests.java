41d240fb59e7797a45dcbea2f16f598fb36a5585
Nikita Prokopenko <guardprokopenko@gmail.com>
2020-12-19 17:10:19 +0300

    Added test which compare lists of users after modification

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserModificationTests.java
@@ -1,7 +1,11 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.UserData;
+
+import java.util.Comparator;
+import java.util.List;
 
 public class UserModificationTests extends TestBase {
 
@@ -10,10 +14,21 @@ public class UserModificationTests extends TestBase {
         if (!app.getUserHelper().isThereAUser()) {
             app.getUserHelper().createUser(new UserData("firstName", "lastName"));
         }
+        List<UserData> before = app.getUserHelper().getUserList();
         app.getNavigationHelper().goToEditUserPage();
-        app.getUserHelper().fillUserForm(new UserData("firstName1", "lastName1"));
+        UserData user = new UserData("firstName1", "lastName1");
+        app.getUserHelper().fillUserForm(user);
         app.getUserHelper().updateUserForm();
         app.getUserHelper().returnToHomePage();
-        app.getSessionHelper().logout();
+        List<UserData> after = app.getUserHelper().getUserList();
+
+        Assert.assertEquals(after.size(), before.size());
+
+        before.remove(before.get(0));
+        before.add(user);
+        Comparator<? super UserData> byId = (u1, u2) -> Integer.compare(u1.getId(), u2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
     }
 }
