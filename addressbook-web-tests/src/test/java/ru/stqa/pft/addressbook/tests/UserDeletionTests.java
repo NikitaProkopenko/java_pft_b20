d9114bd086f5a2de4e19b01425b87513f9198664
Nikita Prokopenko <guardprokopenko@gmail.com>
2020-12-19 17:15:47 +0300

    Added test which compare lists of users after deletion

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserDeletionTests.java
@@ -1,7 +1,10 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.UserData;
+
+import java.util.List;
 
 public class UserDeletionTests extends TestBase {
 
@@ -10,10 +13,14 @@ public class UserDeletionTests extends TestBase {
         if (!app.getUserHelper().isThereAUser()) {
             app.getUserHelper().createUser(new UserData("firstName", "lastName"));
         }
+        List<UserData> before = app.getUserHelper().getUserList();
         app.getNavigationHelper().goToEditUserPage();
         app.getUserHelper().pressDeleteButtonFromForm();
-        app.getSessionHelper().logout();
+        List<UserData> after = app.getUserHelper().getUserList();
 
+        Assert.assertEquals(after.size(), before.size() - 1);
+        before.remove(before.size() - 1);
+        Assert.assertEquals(before, after);
     }
 
     @Test
@@ -21,8 +28,14 @@ public class UserDeletionTests extends TestBase {
         if (!app.getUserHelper().isThereAUser()) {
             app.getUserHelper().createUser(new UserData("firstName", "lastName"));
         }
+        List<UserData> before = app.getUserHelper().getUserList();
         app.getUserHelper().selectUser();
         app.getHomePageHelper().pressDeleteButtonFromHome();
         app.acceptAlert();
+        List<UserData> after = app.getUserHelper().getUserList();
+
+        Assert.assertEquals(after.size(), before.size() - 1);
+        before.remove(before.size() - 1);
+        Assert.assertEquals(before, after);
     }
 }
