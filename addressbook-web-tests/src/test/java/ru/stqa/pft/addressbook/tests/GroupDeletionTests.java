f0ad996f3d6c96d5a3a24f1aab9f5ef4318a9515
Nikita Prokopenko <guardprokopenko@gmail.com>
2020-12-22 14:02:25 +0300

    Switched Lists to Sets in group tests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -5,28 +5,28 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.util.List;
+import java.util.Set;
 
 public class GroupDeletionTests extends TestBase {
 
     @BeforeMethod
     public void ensurePrecondition() {
         app.goTo().groupPage();
-        if(app.group().list().size() == 0) {
+        if(app.group().all().size() == 0) {
             app.group().createGroup(new GroupData().withName("test group"));
         }
     }
 
     @Test
     public void testGroupDeletion() throws Exception {
-        List<GroupData> before = app.group().list();
-        int index = before.size() - 1;
-        app.group().delete(index);
+        Set<GroupData> before = app.group().all();
+        GroupData deletedGroup = before.iterator().next();
+        app.group().delete(deletedGroup);
         app.goTo().groupPage();
 
-        List<GroupData> after = app.group().list();
+        Set<GroupData> after = app.group().all();
         Assert.assertEquals(after.size(), before.size() - 1);
-        before.remove(index);
+        before.remove(deletedGroup);
         Assert.assertEquals(before, after);
 
         app.goTo().homePage();
